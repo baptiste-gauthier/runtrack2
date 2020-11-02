c67437ce7a3624490670d0cf54e9012cd223ed4d
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2020-11-02 15:50:14 +0100

    Début de tableau qui fonctionne à moitié

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -5,13 +5,50 @@
 // $int = 10 ; 
 // $float = 1.58 ; 
 
-$prenom = array ('Mathilde', 'Pierre', 'Amandine', 'Florian') ; 
-
-for ($i = 0 ; $i < 4 ; $i++ )
-{
-    echo $prenom[$i] ; 
-}
-
-
+$tableaux1 = array ('type' => 'booléen', 'Nom' => '$myBool', 'Valeur' => 'True') ; 
+$tableaux2 = array ('type' => 'string', 'Nom' => '$string', 'Valeur' => 'Hello la plateforme') ; 
+$tableaux3 = array ('type' => 'int', 'Nom' => '$int', 'Valeur' => '10') ; 
+$tableaux4 = array ('type' => 'float', 'Nom' => '$float', 'Valeur' => '1.58') ;
 
 ?>
+
+<table style="width:60%; border-collapse:collapse ; border : 1px black solid">
+    <tr>
+        <td>
+            <?php
+                foreach($tableaux1 as $element)
+                {
+                    echo $element . '</br>' ; 
+                }
+            ?>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            <?php
+                foreach($tableaux2 as $element)
+                {
+                    echo $element . '</br>' ; 
+                }
+            ?>
+        </td>
+
+        <td>
+            <?php
+                foreach($tableaux3 as $element)
+                {
+                    echo $element . '</br>' ; 
+                }
+            ?>
+        </td>
+
+        <td>
+            <?php
+                foreach($tableaux4 as $element)
+                {
+                    echo $element . '</br>' ; 
+                }
+            ?>
+        </td>
+    </tr>
+</table>
