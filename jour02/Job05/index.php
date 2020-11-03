2ab3ebeae3d9a96b73a243ef23290656e5a07a28
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2020-11-03 16:07:38 +0100

    version finale

--- jour02/Job05/index.php
+++ jour02/Job05/index.php
@@ -1,10 +1,10 @@
 <?php
 
-function isPrime($number) {
+function nbpremier($number) {
     //boucle de 2 au nombre à tester
     for ($i = 2; $i < $number; $i++) {
-        //test du qutotien de la division
-        if ($number % $i == 0) {
+        //test du quotien de la division
+        if (($number % $i) == 0) {
             return FALSE;
         }
     }
@@ -14,10 +14,10 @@ function isPrime($number) {
 }
  
 // boucle de 0 à 99
-echo '<br>Nombre premier de 0 à 100: ';
-for ($i = 3; $i < 100; $i++) {
-    if (isPrime($i)) {
-        echo $i.' ';
+echo '<br>Nombre premier de 0 à 1000: </br>';
+for ($i = 3; $i < 1000; $i++) {
+    if (nbpremier($i)) {
+        echo $i.'</br> ';
     }
 }
 
